ebab88c98ec8db34a432efdab2c7d10ba7bc9c31
Maloney, Christopher <maloneyc@ncbi.nlm.nih.gov>
2013-09-30 08:56:56 -0400

    Update cron job to shorten my emails

--- test/cronjob.sh
+++ test/cronjob.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/bash
 
 cd /home/maloneyc/git/Klortho/Eutils-JSON/test
+now=`date +"%Y%m%d"`
 
-echo "========================================================" | tee -a ./cron-out.txt
-echo "Running Eutils-JSON tests" | tee -a ./cron-out.txt
-date | tee -a ./cron-out.txt
+outfile_vxml=cron_out_vxml_$now.txt
+echo "Running validate-xml.pl" > $outfile_vxml
+/opt/perl-5.8.8/bin/perl ./validate-xml.pl --tld=try -c >> $outfile_vxml
+echo "======================================================"
+echo validate-xml results
+tail $outfile_vxml
 
-/opt/perl-5.8.8/bin/perl ./validate-xml.pl --tld=try -c  | tee -a ./cron-out.txt
-/opt/perl-5.8.8/bin/perl ./validate-json.pl --tld=try -c | tee -a ./cron-out.txt
+
+outfile_vjson=cron_out_vjson_$now.txt
+echo "Running validate-json.pl" > $outfile_vjson
+/opt/perl-5.8.8/bin/perl ./validate-json.pl --tld=try -c >> $outfile_vjson
+echo "======================================================"
+echo validate-json results
+tail $outfile_vjson
 
 
 
