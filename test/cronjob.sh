9505a96f93f0db5304ecf3c39f35914e064709ba
Maloney, Christopher <maloneyc@ncbi.nlm.nih.gov>
2014-04-04 12:53:04 -0400

    EU-2216 - add non-numeric project id to tests

--- test/cronjob.sh
+++ test/cronjob.sh
@@ -5,7 +5,7 @@ now=`date +"%Y%m%d%H%M%S"`
 
 outfile_vxml=cron_out_vxml_$now.txt
 echo "Running validate-xml.pl" > $outfile_vxml
-/opt/perl-5.8.8/bin/perl ./validate-xml.pl --tld=try -c >> $outfile_vxml
+/opt/perl-5.8.8/bin/perl ./validate-xml.pl -c >> $outfile_vxml
 echo "======================================================"
 echo validate-xml results
 tail $outfile_vxml
@@ -13,7 +13,7 @@ tail $outfile_vxml
 
 outfile_vjson=cron_out_vjson_$now.txt
 echo "Running validate-json.pl" > $outfile_vjson
-/opt/perl-5.8.8/bin/perl ./validate-json.pl --tld=try -c >> $outfile_vjson
+/opt/perl-5.8.8/bin/perl ./validate-json.pl -c >> $outfile_vjson
 echo "======================================================"
 echo validate-json results
 tail $outfile_vjson
